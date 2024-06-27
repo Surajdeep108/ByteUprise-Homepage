fcc18731c75a22f31fc1397fc937d8f51a3b8294
Surajdeep108 <168331587+Surajdeep108@users.noreply.github.com>
2024-06-27 16:57:12 +0530

    Create app.js
    js code

---
+++ app.js
@@ -0,0 +1,8 @@
+const menu = document.querySelectorAll('#mobile-menu');
+const menuLinks = document.querySelectorAll ('.navbar___menu');
+
+menu.addEventListener('click', function() {
+menu.classList.toggle('is-active');
+menuLinks.classList.toggle('active');
+
+});
